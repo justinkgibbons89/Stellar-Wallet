2055bf733808bbe5a9faacee4f52ba53d321fa4e
Justin <justinkgibbons@gmail.com>
2020-02-01 23:29:11 -0800

    up minimum reserve to 20

--- horizon.js
+++ horizon.js
@@ -1,22 +1,31 @@
 const Stellar = require('stellar-sdk');
 const Errors = require('./errors.js')
 
+const DefaultMinimumReserve = "20"
+const MagicFaucetKey = "SBJJ66MG6UWZAXM5GIAJOPULF46WQMV2HQ6GNGOGRARAWIAJSFY5NZGB"
+
+/*
+  Horizon is a client for interacting with Stellar. It exposes basic network functionality for creating accounts and submitting transactions.
+*/
 class Horizon {
 
   //Properties
-  static get minimumReserve() { return "3" }
-  static get magicFaucet() {
-    let sec = "SBJJ66MG6UWZAXM5GIAJOPULF46WQMV2HQ6GNGOGRARAWIAJSFY5NZGB"
-    return Stellar.Keypair.fromSecret(sec)
+  static get minimumReserve() { return DefaultMinimumReserve }
+
+  static get magicFaucet() { 
+    let secretKey = MagicFaucetKey
+    return Stellar.Keypair.fromSecret(secretKey)
   }
 
-  //Initialization
-  constructor() {
-    Stellar.Network.useTestNetwork()
+  //MARK: Initialization
+  constructor(secretKey) {
+    Stellar.Network.useTestNetwork() //live network not supported right now
+    this.secretKey = secretKey
     this.server = new Stellar.Server('https://horizon-testnet.stellar.org');
     this.timeout = 30
   }
 
+  //Only test network currently supported
   selectNetwork(network) {
     switch (network) {
       case "test": Stellar.Network.useTestNetwork()
@@ -24,15 +33,19 @@ class Horizon {
     }
   }
 
-  //Core Requests
+  //MARK: Core Network Requests
+
+  //Returns an account object from a public key
   async account(publicKey) {
     return this.server.loadAccount(publicKey)
   }
 
+  //The network base fee is determined by Stellar and must be fetched.
   async fee() {
     return this.server.fetchBaseFee()
   }
 
+  // Builds a transaction envelope from a set of operations and a public key.
   async transaction(operations, publicKey) {
     let fee = await this.fee()
     let account = await this.account(publicKey)
@@ -46,6 +59,8 @@ class Horizon {
     return transaction
   }
 
+  //Submits a transaction to the network
+  //The transaction must be signed before submission.
   async submit(transaction) {
     try {
       let result = await this.server.submitTransaction(transaction);
@@ -58,7 +73,9 @@ class Horizon {
     }
   }
 
-  //Basic Transactions
+  //MARK: Basic Transactions
+
+  //Only lumens are supported as an asset right now.
   async send(address, source, amount, assetName) {
     var asset = null
     if (assetName == "lumen") { asset = Stellar.Asset.native() }
